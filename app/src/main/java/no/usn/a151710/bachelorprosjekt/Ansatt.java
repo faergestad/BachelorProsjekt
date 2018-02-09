8fec1d0c92fbba83e278a2e51f35c685ffec70b3
Georg Persen <georg.f.persen@gmail.com>
2018-02-09 14:29:08 +0100

    Added all necessary classes

---
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/Ansatt.java
@@ -0,0 +1,13 @@
+package no.usn.a151710.bachelorprosjekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Ansatt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ansatt);
+    }
+}
--- app/src/main/java/no/usn/a151710/bachelorprosjekt/HovedSkjerm.java
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/HovedSkjerm.java
@@ -1,14 +1,62 @@
 package no.usn.a151710.bachelorprosjekt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class HovedSkjerm extends AppCompatActivity {
+
+    private LinearLayout timeRegKnapp;
+    private LinearLayout oppdragKnapp;
+    private LinearLayout kundeKnapp;
+    private LinearLayout ansattKnapp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hoved_skjerm);
 
+        timeRegKnapp = findViewById(R.id.timeRegKnapp);
+        oppdragKnapp = findViewById(R.id.oppdragKnapp);
+        kundeKnapp = findViewById(R.id.kundeKnapp);
+        ansattKnapp = findViewById(R.id.ansattKnapp);
+
+        timeRegKnapp.setOnClickListener(onClickListener);
+        oppdragKnapp.setOnClickListener(onClickListener);
+        kundeKnapp.setOnClickListener(onClickListener);
+        ansattKnapp.setOnClickListener(onClickListener);
+
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()) {
+                case R.id.timeRegKnapp:
+                    Intent startV1 = new Intent(v.getContext(), TimeRegistrering.class);
+                    startActivity(startV1);
+                    break;
+
+                case R.id.oppdragKnapp:
+                    Intent startV2 = new Intent(v.getContext(), Oppdrag.class);
+                    startActivity(startV2);
+                    break;
+
+                case R.id.kundeKnapp:
+                    Intent startV3 = new Intent(v.getContext(), Kunde.class);
+                    startActivity(startV3);
+                    break;
+
+                case R.id.ansattKnapp:
+                    Intent startV4 = new Intent(v.getContext(), Ansatt.class);
+                    startActivity(startV4);
+                    break;
+            }
+
+        }
+    };
+
 }
