0a51cdfcee4df0e4e2c0f7d48f14ea80107ca4f7
Georg Persen <georg.f.persen@gmail.com>
2018-02-09 11:36:49 +0100

    Login screen with added button to remember user + fixed typos

---
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/HovedSkjerm.java
@@ -0,0 +1,14 @@
+package no.usn.a151710.bachelorprosjekt;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HovedSkjerm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hoved_skjerm);
+
+    }
+}
--- app/src/main/java/no/usn/a151710/bachelorprosjekt/MainActivity.java
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/MainActivity.java
@@ -1,13 +1,37 @@
 package no.usn.a151710.bachelorprosjekt;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText skrivBrukernavn;
+    private EditText skrivPassord;
+    private Switch huskMeg;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        skrivBrukernavn = findViewById(R.id.userField);
+        skrivPassord = findViewById(R.id.passField);
+        huskMeg = findViewById(R.id.rememberSwitch);
+        loginBtn = findViewById(R.id.loginBtn);
     }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent fraLogin = new Intent(v.getContext(), HovedSkjerm.class);
+            startActivity(fraLogin);
+        }
+    };
+
 }
