4974741d2157988c9fc74095fbb6962084d50feb
Kim Allergodt <kim.allergodt@outlook.com>
2018-03-21 13:07:46 +0100

    la til recyclerview for kunder

--- app/src/main/java/no/usn/a151710/bachelorprosjekt/Kunde.java
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/Kunde.java
@@ -11,26 +11,19 @@ import java.util.ArrayList;
 
 public class Kunde {
 
-    private int id;
-    private String customerName, address, mail;
+    private String customerName, mail;
 
     private static String TABELL_NAVN = "customer";
-    private static String KOL_ID = "cID";
     private static String KOL_CUSTOMER_NAME = "cNAME";
-    private static String KOL_ADDRESS = "address";
     private static String KOL_MAIL = "mail";
 
-    public Kunde(int id, String customerName, String address, String mail) {
-        this.id = id;
+    public Kunde(String customerName, String mail) {
         this.customerName = customerName;
-        this.address = address;
         this.mail = mail;
     }
 
     public Kunde(JSONObject jsonKunde) {
-        this.id = jsonKunde.optInt(KOL_ID);
         this.customerName = jsonKunde.optString(KOL_CUSTOMER_NAME);
-        this.address = jsonKunde.optString(KOL_ADDRESS);
         this.mail = jsonKunde.optString(KOL_MAIL);
     }
 
@@ -52,23 +45,12 @@ public class Kunde {
         JSONObject jsonKunde = new JSONObject();
 
         try {
-            jsonKunde.put(KOL_ID, this.id);
             jsonKunde.put(KOL_CUSTOMER_NAME, this.customerName);
-            jsonKunde.put(KOL_ADDRESS, this.address);
-            jsonKunde.put(KOL_ADDRESS, this.address);
             jsonKunde.put(KOL_MAIL, this.mail);
         } catch (JSONException e) {
             return null;
         }
         return jsonKunde;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getCustomerName() {
@@ -77,14 +59,6 @@ public class Kunde {
 
     public void setCustomerName(String customerName) {
         this.customerName = customerName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
     }
 
     public String getMail() {
---
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/RecyclerViewKundeAdapter.java
@@ -0,0 +1,71 @@
+package no.usn.a151710.bachelorprosjekt;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import java.util.List;
+
+/**
+ * Created by GeorgPersen on 02.03.2018.
+ */
+
+public class RecyclerViewKundeAdapter extends RecyclerView.Adapter<RecyclerViewKundeAdapter.ViewHolder> {
+
+    Context context;
+    List<Kunde> kunde;
+
+    public RecyclerViewKundeAdapter(List<Kunde> kunde, Context context) {
+
+        super();
+
+        this.kunde = kunde;
+        this.context = context;
+
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.recylerview2_items, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+        Kunde kunde1 = kunde.get(position);
+
+        holder.CustomerTextView.setText(kunde1.getCustomerName());
+        holder.MailTextView.setText(kunde1.getMail());
+
+
+    }
+
+    @Override
+    public int getItemCount() { return kunde.size(); }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        public TextView CustomerTextView;
+        public TextView MailTextView;
+
+        public ViewHolder(View itemView) {
+
+            super(itemView);
+
+            CustomerTextView = itemView.findViewById(R.id.kundeText4);
+            MailTextView = itemView.findViewById(R.id.kundeText4);
+
+        }
+
+        @Override
+        public void onClick(View view) {
+
+        }
+    }
+}
