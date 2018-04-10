68ba0f455e78960d9f6207dd07bfb6c00712816d
Georg Persen <georg.f.persen@gmail.com>
2018-04-10 22:44:58 +0200

    Lagt til stoppeklokke (Chronometer.class)

--- app/src/main/java/no/usn/a151710/bachelorprosjekt/TimeRegistrering.java
+++ app/src/main/java/no/usn/a151710/bachelorprosjekt/TimeRegistrering.java
@@ -1,25 +1,20 @@
 package no.usn.a151710.bachelorprosjekt;
 
 import android.Manifest;
-import android.app.LauncherActivity;
-import android.app.ProgressDialog;
 import android.content.Context;
-        import android.content.pm.PackageManager;
-        import android.graphics.Color;
-        import android.location.Address;
-        import android.location.Criteria;
-        import android.location.Geocoder;
-        import android.location.Location;
-        import android.location.LocationManager;
-import android.nfc.Tag;
-import android.os.AsyncTask;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Build;
-        import android.os.Bundle;
-        import android.support.v4.app.ActivityCompat;
-        import android.support.v4.app.FragmentActivity;
-        import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,77 +22,117 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.VolleyLog;
-import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.common.ConnectionResult;
-        import com.google.android.gms.common.api.GoogleApiClient;
-        import com.google.android.gms.location.LocationListener;
-        import com.google.android.gms.location.LocationRequest;
-        import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
-        import com.google.android.gms.maps.GoogleMap;
-        import com.google.android.gms.maps.OnMapReadyCallback;
-        import com.google.android.gms.maps.SupportMapFragment;
-        import com.google.android.gms.maps.model.Circle;
-        import com.google.android.gms.maps.model.CircleOptions;
-        import com.google.android.gms.maps.model.LatLng;
-        import com.google.android.gms.maps.model.Marker;
-        import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
-        import java.util.Locale;
-import java.util.Map;
-
 
 public class TimeRegistrering extends FragmentActivity implements OnMapReadyCallback,
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+        LocationListener, View.OnClickListener {
 
-    private static final String TAG = "Feil: ";
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+    private static final String TAG = "Timeregistrering: ";
+    private static final String URL_DATA = "http://192.168.10.172/BachelorProsjektNettsted/api.php/latlong?&transform=1";
+    private GoogleMap mMap;
+    private TextView timer;
+    private TextView antTimer;
+    private ImageButton btnStart;
+    private ImageButton btnStop;
+    private no.usn.a151710.bachelorprosjekt.Chronometer mChronometer;
+    private Thread mThreadChrono;
+    private Context mContext;
     GoogleApiClient mGoogleApiClient;
     Location mLastLocation;
     Marker mCurrLocationMarker;
     LocationRequest mLocationRequest;
     LatLng huset = new LatLng(59.414349, 9.058793);
-    private GoogleMap mMap;
-    private ProgressDialog pDialog;
     ArrayList<String> latList = new ArrayList<>();
     ArrayList<String> lngList = new ArrayList<>();
-    private static final String URL_DATA = "http://192.168.10.172/BachelorProsjektNettsted/api.php/latlong?&transform=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time_registrering);
+        mContext = this;
+
+        antTimer = findViewById(R.id.antTimer);
+        timer = findViewById(R.id.timer);
+        btnStart = findViewById(R.id.start);
+        btnStop = findViewById(R.id.stop);
+
 
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
+
         SupportMapFragment mapFragment = (SupportMapFragment)
                 getSupportFragmentManager()
                         .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mChronometer == null) {
+                    mChronometer = new no.usn.a151710.bachelorprosjekt.Chronometer(mContext);
+                    mThreadChrono = new Thread(mChronometer);
+                    mThreadChrono.start();
+                    mChronometer.start();
+                }
+
+            }
+        });
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mChronometer != null) {
+                    mChronometer.stop();
+                    mThreadChrono.interrupt();
+                    mThreadChrono = null;
+                    // Lagt til for å sjekke lagring av tid i string-format
+                    //antTimer.setText(String.valueOf(timer.getText()));
+                    Toast.makeText(mContext, "Du har logget: " + String.valueOf(timer.getText())
+                            + " timer", Toast.LENGTH_LONG).show();
+                    mChronometer = null;
+                } else {
+                    oppdaterTimerText("00:00:00");
+                }
+            }
+        });
+
         hentKoordinater();
     }
+
+    public void oppdaterTimerText(final String tid) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                timer.setText(tid);
+            }
+        });
+    }
+
 
     public void hentKoordinater() {
         JsonObjectRequest jsObjRequest = new JsonObjectRequest
@@ -145,20 +180,13 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-//Initialize Google Play Services
+        //Initialize Google Play Services
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)
                     == PackageManager.PERMISSION_GRANTED) {
                 buildGoogleApiClient();
                 mMap.setMyLocationEnabled(true);
-                /* Mal for å lage sirkler på kartet
-                Circle circle = mMap.addCircle(new CircleOptions()
-                        .center(huset)
-                        .radius(10)
-                        .strokeColor(0x440000FF)
-                        .strokeWidth(2)
-                        .fillColor(0x220000FF));*/
             }
 
         } else {
@@ -180,7 +208,7 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
     public void onConnected(Bundle bundle) {
         mLocationRequest = LocationRequest.create();
         mLocationRequest.setInterval(1000);
-        mLocationRequest.setFastestInterval(1000);
+        mLocationRequest.setFastestInterval(500);
         mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
@@ -200,7 +228,7 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
         if (mCurrLocationMarker != null) {
             mCurrLocationMarker.remove();
         }
-//Showing Current Location Marker on Map
+        //Showing Current Location Marker on Map
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
         markerOptions.position(latLng);
@@ -220,33 +248,10 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
 // for ActivityCompat#requestPermissions for more details.
             return;
         }
-        Location locations = locationManager.getLastKnownLocation(provider);
-        List<String> providerList = locationManager.getAllProviders();
-        if (null != locations && null != providerList && providerList.size() > 0) {
-            double longitude = locations.getLongitude();
-            double latitude = locations.getLatitude();
-            Geocoder geocoder = new Geocoder(getApplicationContext(),
-                    Locale.getDefault());
-            try {
-                List<Address> listAddresses = geocoder.getFromLocation(latitude,
-                        longitude, 1);
-                if (null != listAddresses && listAddresses.size() > 0) {
-// Here we are finding , whatever we want our marker to show when clicked
-                    String state = listAddresses.get(0).getAdminArea();
-                    String country = listAddresses.get(0).getCountryName();
-                    String subLocality = listAddresses.get(0).getSubLocality();
-                    markerOptions.title("" + latLng + "," + subLocality + "," + state
-                            + "," + country);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-        //mCurrLocationMarker = mMap.addMarker(markerOptions);
-//move map camera
+        //move map camera
         CameraUpdate yourLocation = CameraUpdateFactory.newLatLngZoom(latLng, 17);
         mMap.animateCamera(yourLocation);
-//this code stops location updates
+        //this code stops location updates
         if (mGoogleApiClient != null) {
             LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient,
                     this);
@@ -261,18 +266,18 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-// Asking user if explanation is needed
+            // Asking user if explanation is needed
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                     Manifest.permission.ACCESS_FINE_LOCATION)) {
-// Show an explanation to the user *asynchronously* -- don't block
-// this thread waiting for the user's response! After the user
-// sees the explanation, try again to request the permission.
-//Prompt the user once explanation has been shown
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                //Prompt the user once explanation has been shown
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_LOCATION);
             } else {
-// No explanation needed, we can request the permission.
+                // No explanation needed, we can request the permission.
                 ActivityCompat.requestPermissions(this,
                         new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         MY_PERMISSIONS_REQUEST_LOCATION);
@@ -288,11 +293,11 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
                                            String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
-// If request is cancelled, the result arrays are empty.
+                // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-// permission was granted. Do the
-// contacts-related task you need to do.
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
                     if (ContextCompat.checkSelfPermission(this,
                             Manifest.permission.ACCESS_FINE_LOCATION)
                             == PackageManager.PERMISSION_GRANTED) {
@@ -305,8 +310,12 @@ public class TimeRegistrering extends FragmentActivity implements OnMapReadyCall
                     Toast.makeText(this, "permission denied",
                             Toast.LENGTH_LONG).show();
                 }
-                return;
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Måtte implementeres utenfor onCreate
     }
 }
